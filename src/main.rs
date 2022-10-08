9d6e6431943087d91ed262f98157c6dbd540cd6c
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2022-10-08 15:47:54 -0400

    Save notible words to text file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use std::fs::File;
 
 fn filename_to_string(s: &str) -> io::Result<String> {
@@ -29,18 +29,20 @@ fn main() {
 
     let mut scores: Vec<f32> = vec![];
 
-    // let mut out = File::create("words.txt").expect("Couldn't create output file");
+    let mut out = File::create("words.txt").expect("Couldn't create output file");
+    let cool_words_score: f32 = 3.0;
+    out.write_fmt(format_args!("Saving words that score at least {}...\n", cool_words_score)).expect("Couldn't write to output file");
 
-    let mut i = 0;
     let mut best_word = 0;
     let mut best_fair_word = 0;
-
+    
+    let mut i = 0;
     while i < words.len() {
         let score: f32 = (words[i].len() as f32)/(get_unique_letters(words[i]) as f32);
         scores.push(score);
 
-        if score >= 3.0 {
-            println!("{}", words[i]);
+        if score >= cool_words_score {
+            out.write_fmt(format_args!("{}, len = {:?}, ui = {:?}, score = {:?}\n", words[i], words[i].len(), get_unique_letters(words[i]), score)).expect("Couldn't write to output file");
         }
 
         if score > scores[best_word] {
@@ -56,7 +58,7 @@ fn main() {
 
     println!("Done! Best word overall was {}. Best REAL word was {}, {:?} letters long, {:?} unique letters, giving a score of {:?}!", words[best_word], words[best_fair_word], words[best_fair_word].len(), get_unique_letters(words[best_fair_word]), scores[best_fair_word]);
 
-    // for word in in_prog.iter() {
+    // for word in best_wordse {
     //     for i in 0..word.len() {
     //         for l in 0..word_len {
     //             // print!("{}", word[i] as char);
